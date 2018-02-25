e59ad0c0197d637747a7032cf8a77c5ddca74133
WarrG3X <warrier.abhishek@gmail.com>
2018-02-25 17:24:48 +0530

    Balance now publishes motor data for logging

--- src/balance.py
+++ src/balance.py
@@ -5,6 +5,7 @@ import pypot.dynamixel
 from math import pi,acos,tan,log,degrees,radians
 from geometry_msgs.msg import Vector3Stamped
 from sensor_msgs.msg import Imu
+from humanoid_balance.msg import botinfo
 
 
 #Constants
@@ -18,6 +19,7 @@ SPEED = 1023
 LOCK = 19
 AXIS1 = 'x'
 AXIS2 = 'y'
+AXIS3 = 'z'
 FLAG = None
 
 #Variables
@@ -58,7 +60,21 @@ def get_angles(t):
     angleC = -degrees(acos((b**2 + h**2 +(d**2)*(tan(t)**2) -c**2-2*h*d*tan(t))/(2*b*(h-d*tan(t)))))
     return angleA,angleB,angleC
 
+def publish_log(angles,yaw,pitch,roll):
+    message = botinfo()
+    message.yaw = yaw
+    message.pitch = pitch
+    message.roll = roll
+    message.motor11 = angles[11]
+    message.motor12 = angles[12]
+    message.motor13 = angles[13]
+    message.motor14 = angles[14]
+    message.motor15 = angles[15]
+    message.motor16 = angles[16]
+    message.motor17 = angles[17]
+    message.motor18 = angles[18]
 
+    pub.publish(message)
 
 def leftleg(t,debug=True):
     global angles
@@ -74,7 +90,7 @@ def leftleg(t,debug=True):
 
 
     angleA,angleB,angleC = get_angles(t)
-    angles ={12:angleB,14:angleA,15:angles[15],16:angleC,17:factor*theta,18:factor*theta}
+    angles ={11:angles[11],12:angleB,13:angles[13],14:angleA,15:angles[15],16:angleC,17:factor*theta,18:factor*theta}
 
 def rightleg(t,debug=True):
     global angles
@@ -89,7 +105,7 @@ def rightleg(t,debug=True):
         factor = 1
 
     angleA,angleB,angleC = get_angles(abs(t))
-    angles = {11:-angleB,13:-angleA,15:-angleC,16:angles[16],17:factor*theta,18:factor*theta}
+    angles = {11:-angleB,12:angles[12],13:-angleA,14:angles[14],15:-angleC,16:angles[16],17:factor*theta,18:factor*theta}
 
 def sagittal_balance(phi):
     #Phi is in Degrees
@@ -116,11 +132,14 @@ def get_rpy(data):
     global FLAG
     t = getattr(data.vector,AXIS1)
     p = getattr(data.vector,AXIS2)
+    s = getattr(data.vector,AXIS3)
     sign = t/abs(t)
     t = sign*abs(pi-abs(t))
     theta = degrees(t)
     phi = degrees(p)
-    print phi
+    psi = degrees(s)
+    #print phi
+
     if theta > 0 and theta < 25:
         leftleg(t,debug=False)
         FLAG = 'LEFT'
@@ -140,13 +159,14 @@ def get_rpy(data):
         pass
 
     print angles
+    publish_log(angles,psi,phi,theta)
     dxl_io.set_goal_position(angles)
 
-
 if __name__ == '__main__':
-    rospy.init_node('humanoid_balance',anonymous=False)
+    rospy.init_node('balance',anonymous=False)
     initialize_dxl()
     initialize_robot()
     raw_input("Begin?")
+    pub = rospy.Publisher('robotlog',botinfo,queue_size=10)
     rospy.Subscriber("/imu/rpy/filtered",Vector3Stamped,get_rpy)
     rospy.spin()
